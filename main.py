b96895f874f397e536b52fac89ed46a9820c2c15
Jared Lieberman <jliebs20@gmail.com>
2020-07-23 14:07:57 -0400

    Get first page captions

--- main.py
+++ main.py
@@ -3,32 +3,43 @@ from bs4 import BeautifulSoup
 import requests
 
 from urllib.request import urlopen, Request
+import re
 
 
 
 
+
+
+def get_page_captions(reg_url):
+
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+	req = Request(url=reg_url, headers=headers) 
+	html = urlopen(req).read() 
+
+	soup = BeautifulSoup(html, "html.parser")
+	#print(soup.text)
+
+	match = re.findall('Winning caption:(.+?)”', soup.text, flags=re.IGNORECASE)
+
+	print(match)
+	print(len(match))
+
+	return(match)
 
 
 
 
 def scrape_captions():
 	
+	#https://micromismanagement.com/tag/new-yorker/page/2/
 
-	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-	reg_url = "https://micromismanagement.com/tag/new-yorker/"
-	req = Request(url=reg_url, headers=headers) 
-	html = urlopen(req).read() 
-	print(html)
+	page_num = 1
+	
+	reg_url = "https://micromismanagement.com/tag/new-yorker/page/1/"
+	captions_list = get_page_captions(reg_url)
 
-	#url = "https://micromismanagement.com/tag/new-yorker/"
-	#req = requests.get(url)
 
-	#print(req)
 
-	#soup = BeautifulSoup(req.text, "html.parser")
-	#print(type(soup.string))
-
-	#print(soup.title)
 
 
 
